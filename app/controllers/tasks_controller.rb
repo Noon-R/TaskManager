6c69059318c2d1f752a2aa6bbb4e1c2cfa1753b5
Noon-R <r1234psp@yahoo.co.jp>
2020-05-01 19:04:59 +0900

    Add delete and Edit Methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,9 +35,23 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Editted Task!!'
+      redirect_to @task
+    else
+      flash.now[:danger] = "Didn't Edit "
+      render :new
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Deleteted Task'
+    redirect_to tasks_path
   end  
 
   private
